d8f81a93bd34e82b85a8c8f890eac5ce6ab5220e
Ramil Arthan <hengzagod@gmail.com>
2019-10-21 20:38:42 +0700

    update

--- content/script.js
+++ content/script.js
@@ -1,4 +1,12 @@
 var tap = 1;
+var map = document.querySelector('#map');
+var borad = map.getContext("2d");
+var map_image = new Image();
+map_image.src = "../img/map.png";
+
+var width = map.style.width;
+var height = map.style.height;
+
 function change1(){
     var content = document.querySelector('#tap-bar');
     var slide = document.querySelector('#slide');
@@ -13,3 +21,10 @@ function change1(){
         tap = 1;
     }
 }
+
+setInterval(function(){
+    width = screen.availWidth+"px";
+    heigt = screen.availHeight+"px";
+    borad.drawImage(map_image, 0, 0, width, height);
+
+}, 1000);
