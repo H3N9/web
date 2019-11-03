b1e66668d6a1576652ed57e5e9c4fcba5da86a8b
Ramil Arthan <42642459+H3N9@users.noreply.github.com>
2019-11-04 04:13:57 +0700

    effect slide

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
+window.onscroll = function() {scorll()};
+
+
+
 function myMenu() {
     var x = document.getElementById("menu");
-    var h1 = document.getElementById("ham1")
-    var h2 = document.getElementById("ham2")
-    var h3 = document.getElementById("ham3")
+    var h1 = document.getElementById("ham1");
+    var h2 = document.getElementById("ham2");
+    var h3 = document.getElementById("ham3");
     
-    if (x.style.left === "50%") {
-        x.style.left = "100%";
-        h1.style.transform = "rotate(0)" 
+
+    if (x.style.height == "35vw") {
+        x.style.height = "0";
+        h1.style.transform = "rotate(0)";
         h2.style.opacity = "100";
-        h3.style.transform = "rotate(0)" 
+        h3.style.transform = "rotate(0)";
 
     }else{
-        x.style.left = "50%";
-        h1.style.transform = "rotate(-45deg) translate(-1vw, 0.5vw)" 
+        x.style.height = "35vw";
+        h1.style.transform = "rotate(-45deg) translate(-1.8vw, 0.5vw)";
         h2.style.opacity = "0";
-        h3.style.transform = "rotate(45deg) translate(-1vw, -0.5vw)" 
-
+        h3.style.transform = "rotate(45deg) translate(-1.8vw, -0.5vw)";
     }
-  }
+}
+
+
+function scorll(){
+    // line slide
+    var x = document.body.scrollTop;
+    var dash = document.querySelector("#line-dashed");
+    var board = document.querySelector("#board");
+    var rect = board.getBoundingClientRect();
+    
+    if(x>rect.height-screen.availHeight-50){
+        dash.style.width = "90%";
+    }else{
+        dash.style.width = "0";
+    }
+
+    //show rush
+    var rush = document.querySelector("#rush");
+    var bg1 = document.querySelector("#bg1");
+    var rect1 = bg1.getBoundingClientRect();
+    if(x>rect1.height){
+        rush.style.display = "block";
+    }
+    else{
+        rush.style.display = "none";
+    }
+
+}
