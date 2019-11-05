67534ef4ffcee536ec8f730cbed2fd420647dbec
Ramil Arthan <42642459+H3N9@users.noreply.github.com>
2019-11-05 12:15:19 +0700

    smart phone update

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-window.onscroll = function() {scorll()};
-
-
-
 function myMenu() {
     var x = document.getElementById("menu");
     var h1 = document.getElementById("ham1");
@@ -22,14 +18,24 @@ function myMenu() {
         h3.style.transform = "rotate(45deg) translate(-1.8vw, -0.5vw)";
     }
 }
+setInterval(function loop(){
+    if(screen.availWidth>1500){
+        window.onscroll = function() {scroll();}
+    }
+    else
+        scroll();
+},100);
 
 
-function scorll(){
+
+function scroll(){
     // line slide
-    var x = document.body.scrollTop;
+    
     var dash = document.querySelector("#line-dashed");
     var board = document.querySelector("#board");
+    var x = document.body.scrollTop;
     var rect = board.getBoundingClientRect();
+
     
     if(x>rect.height-screen.availHeight-50){
         dash.style.width = "90%";
@@ -48,4 +54,16 @@ function scorll(){
         rush.style.display = "none";
     }
 
+    //show row
+    var row = document.getElementsByClassName("row");
+    var rect3 = document.getElementById('content').getBoundingClientRect();
+    var rect4 = document.getElementById('bind').getBoundingClientRect();
+    var rect5 = document.getElementsByClassName("row")[0].getBoundingClientRect();
+    var all = rect3.height+rect1.height+rect4.height;
+    if(x>all-screen.availHeight){
+        row[0].style.opacity = "1";
+    }
+    if(x>(all+rect5.height+rect4.height)-screen.availHeight){
+        row[1].style.opacity = "1";
+    }
 }
