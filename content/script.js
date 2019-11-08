a118d08860048aac84c2a3b59087493f8acf16fe
Ramil Arthan <hengzagod@gmail.com>
2019-11-09 00:05:44 +0700

    all

--- content/script.js
+++ content/script.js
@@ -1,8 +1,5 @@
 var tap = 1;
-var map = document.querySelector('#map');
-var borad = map.getContext("2d");
-var map_image = new Image();
-map_image.src = "../img/map.png";
+
 
 function change1(){
     var content = document.querySelector('#tap-bar');
@@ -18,9 +15,3 @@ function change1(){
         tap = 1;
     }
 }
-
-setInterval(function(){
-    map.style.width = screen.availWidth+"px";
-    map.style.height = screen.availHeight+"px";
-
-}, 1000);
---
+++ content/ww1/slid.js
@@ -0,0 +1,10 @@
+function inover(x){
+  var text = document.getElementsByClassName("time")[x];
+  text.style.marginTop = "2vw";
+  text.style.fontSize = "1.5vw";
+}
+function outover(x){
+  var text = document.getElementsByClassName("time")[x];
+  text.style.marginTop = "3vw";
+  text.style.fontSize = "1.2vw";
+}
